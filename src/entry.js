45d75591599bea98da7095dc0bfa151b0d560e51
N0t-Eb1 <gangninjax2030@gmail.com>
2026-02-09 03:58:22 -0800

    Remove tilt from hero image and fix body overflow due to glitch effect

--- src/entry.js
+++ src/entry.js
@@ -1,13 +1,4 @@
 import "./css/style.css";
-import VanillaTilt from "vanilla-tilt";
 import { PowerGlitch } from "powerglitch";
-
-VanillaTilt.init(document.querySelector(".hero-img__container"), {
-    reverse: true,
-    max: 25,
-    glare: true,
-    "max-glare": 0.05,
-    speed: 800,
-});
 
 PowerGlitch.glitch(".hero-img__img");
